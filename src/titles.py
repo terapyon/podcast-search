b1bf02bb9cef0d09ec6060cfe7c3f5d62a3bbc80
Manabu TERADA <terada@cmscom.jp>
2025-01-02 13:20:01 +0900

    length, audio を追加した

--- src/titles.py
+++ src/titles.py
@@ -16,7 +16,7 @@ def parse_csv(filename: str | Path) -> pd.DataFrame:
     df = pd.read_csv(filename,
                     header=None,
                     )
-    df.columns = ["id", "day_import", "date_import", "title_import"]
+    df.columns = ["id", "day_import", "date_import", "length", "audio", "title_import"]
     df["datetime"] = pd.to_datetime(df["date_import"], format=" %d %b %Y %H:%M:%S %Z")
     df["date"] = df.loc[:, "datetime"].apply(lambda x: parse_ja_date(x))
     df["title"] = df.loc[:, "title_import"].astype("string")
@@ -25,9 +25,9 @@ def parse_csv(filename: str | Path) -> pd.DataFrame:
 
 def ignore_columns(df: pd.DataFrame, limit: str | None) -> pd.DataFrame:
     if limit is not None:
-        df_out = df.loc[df.loc[:, "datetime"] > limit, ["id", "date", "title"]]
+        df_out = df.loc[df.loc[:, "datetime"] > limit, ["id", "date", "length", "audio", "title"]]
     else:
-        df_out = df.loc[:, ["id", "date", "title"]]
+        df_out = df.loc[:, ["id", "date", "length", "audio", "title"]]
     return df_out
 
 
