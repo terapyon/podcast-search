9d69587558ec6a52a7d4777844791939d32c3c35
Manabu TERADA <terada@cmscom.jp>
2025-01-02 18:31:21 +0900

    insert podcast teble

--- src/store.py
+++ src/store.py
@@ -1,21 +1,26 @@
+from pathlib import Path
 import duckdb
 from config import DUCKDB_FILE
+
+
+HERE = Path(__file__).parent
+STORE_DIR = HERE.parent / "store"
 
 
 def create_table():
     conn = duckdb.connect(DUCKDB_FILE)
     podcasts_create = """CREATE TABLE podcasts (
-        id INTEGER PRIMARY KEY, 
-        title TEXT, date DATE, guests TEXT[], length INTEGER, audio TEXT
+        id BIGINT PRIMARY KEY, 
+        title TEXT, date DATE, guests TEXT[], length BIGINT, audio TEXT
         );
     """
     episodes_create = """CREATE TABLE episodes (
-        id INTEGER, part INTEGER, start INTERVAL, end_ INTERVAL, text TEXT,
+        id BIGINT, part BIGINT, start INTERVAL, end_ INTERVAL, text TEXT,
         PRIMARY KEY (id, part)
         );
     """
     embeddings_create = """CREATE TABLE embeddings (
-        id INTEGER, part INTEGER, embedding FLOAT[1024],
+        id BIGINT, part BIGINT, embedding FLOAT[1024],
         PRIMARY KEY (id, part)
         );
     """
@@ -27,12 +32,25 @@ def create_table():
     print("Tables created.")
 
 
+def insert_podcast():
+    conn = duckdb.connect(DUCKDB_FILE)
+    sql = """INSERT INTO podcasts
+        SELECT id, title, date, [], length, audio
+          FROM read_parquet(?);
+    """
+    conn.execute(sql, [str(STORE_DIR / 'podcast-title-list-202301-202501.parquet')])
+    conn.commit()
+    conn.close()
+
+
 if __name__ == "__main__":
     import sys
     args = sys.argv
     if len(args) == 2:
         if args[1] == "create":
             create_table()
+        elif args[1] == "podcastinsert":
+            insert_podcast()
     else:
         print("Usage: python store.py create")
         sys.exit(1)
